10f418ab455f672b506b76591f4d36437f6b6981
Eric McIntyre <emcintyre@cyberscout.com>
2018-01-22 17:26:14 -0700

    Removed last of Groovy code (test)

---
+++ friendly-customer-app/src/test/java/com/notaneye/demo/friendly/FriendlyCustomerApplicationTests.java
@@ -0,0 +1,19 @@
+package com.notaneye.demo.friendly;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendlyCustomerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
